b49e2ffd482809cf42441ff60449ec32f7e96af1
Bernard Faria <bernardxf@gmail.com>
2018-05-13 09:57:33 -0300

    Processing Authentication Credentials

--- auth/src/App.js
+++ auth/src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
 import firebase from 'firebase';
-import { Header } from './components/common';
+import { Header, Button, Spinner } from './components/common';
 import LoginForm from './components/LoginForm';
 
 class App extends Component {
+  state = { loggedIn: null }
+
   componentWillMount() {
     firebase.initializeApp({
       apiKey: "AIzaSyD0HfUO3oktqgNWfPQBtLPqM3h_iAqQk0g",
@@ -14,13 +16,36 @@ class App extends Component {
       storageBucket: "auth-6fe9f.appspot.com",
       messagingSenderId: "267935929953"
     });
+
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ loggedIn: true });
+      } else {
+        this.setState({ loggedIn: false });
+      }
+    });
+  }
+
+  renderContent() {
+    switch (this.state.loggedIn) {
+      case true:
+        return (
+          <Button onPress={ () => firebase.auth().signOut() }>
+            Log Out
+          </Button>
+        );
+      case false:
+        return <LoginForm />;
+      default:
+        return <Spinner size='large' />;
+    }
   }
 
   render() {
     return (
       <View>
         <Header headerText="Authentication" />
-        <LoginForm />
+        { this.renderContent() }
       </View>
     );
   }
